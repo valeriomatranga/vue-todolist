069af5dfd4aa78ba7aa43e22ae2b67431aadae7e
valeriomatranga <matrangav@gmail.com>
2021-05-20 11:01:25 +0200

    correzzione

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,27 +18,37 @@ const app = new Vue({
     methods:{
         insertMessage: function(){
             if(this.promemoria.length != ""){
-                this.tasks.push(this.promemoria);
+                this.tasks.unshift(this.promemoria);
                 this.promemoria = ''
             }else{
-                alert('Devi inserire qualcosa!');
+                alert('Devi inserire qualcosa!😉');
             }
         },
 
         remove: function(index){
-            this.deletTasks.push(this.tasks[index])
+            this.deletTasks.unshift(this.tasks[index])
             this.tasks.splice(index,1);
         },
 
         complete: function(index){
-            this.completeTasks.push(this.tasks[index])
-            
+            this.completeTasks.unshift(this.tasks[index])
+            this.tasks.splice(index,1);
         },
 
         restore: function(index){
-            this.tasks.push(this.deletTasks[index])
+            this.tasks.unshift(this.deletTasks[index])
             this.deletTasks.splice(index,1);
-        }
+        },
+
+        delet: function(){
+            const cancella = confirm('Vuoi cancellare davvero tutto!')
+            
+            if(cancella){
+                this.deletTasks = [];
+            }
+        },
+
+
 
     },
 
